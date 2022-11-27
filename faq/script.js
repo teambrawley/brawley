3c56100e3b87a774cd962779a1de605199f5bfef
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-27 18:41:15 +0800

    Update script.js

--- faq/script.js
+++ faq/script.js
@@ -1,8 +1,19 @@
 fetch("/faq.json")
-.then(response => response.text())
+.then(response => response.json())
 .then(structuredDataText => {
   const script = document.createElement('script');
   script.setAttribute('type', 'application/ld+json');
   script.textContent = structuredDataText;
   document.head.appendChild(script);
+  const about = document.getElementById("about");
+  const faqs = structuredDataText.mainEntity;
+  faqs.forEach(e => {
+    const question = document.createElement('h1');
+    const answer = document.createElement('p');
+    question.textContent = e.name;
+    answer.textContent = e.acceptedAnswer.text;
+    about.appendChild(question);
+    about.appendChild(answer);
+    about.appendChild(document.createElement('br'));
+  })
 });
