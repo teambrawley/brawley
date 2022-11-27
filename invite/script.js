5ae70705fc400ca35a229816fe98057f5fcb1e08
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-27 18:16:51 +0800

    Delete script.js

--- invite/script.js
+++
@@ -1,8 +0,0 @@
-fetch("/faq.json")
-.then(response => response.text())
-.then(structuredDataText => {
-  const script = document.createElement('script');
-  script.setAttribute('type', 'application/ld+json');
-  script.textContent = structuredDataText;
-  document.head.appendChild(script);
-});
